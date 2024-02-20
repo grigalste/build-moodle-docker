ba06efddefb6baf59576e4ba6e686ccfe6ea3252
Alex Ivanov <ivanov.alexey@onlyoffice.com>
2024-02-20 12:34:47 +0300

    Update run-server.sh

--- without_code/run-server.sh
+++ without_code/run-server.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+if [ ${DEBUG_SCRIPT} == "true"]; then
+    set -x
+fi
+
 SQL_DB_TYPE=${SQL_DB_TYPE:-mariadb};
 SQL_DB_HOST=${SQL_DB_HOST:-localhost};
-SQL_DB_NAME=${SQL_DB_NAME:-moodledb};
-SQL_DB_USER=${SQL_DB_USER:-moodle};
-SQL_DB_PASS=${SQL_DB_PASS:-moodle};
+SQL_DB_NAME=${SQL_DB_NAME:-db};
+SQL_DB_USER=${SQL_DB_USER:-user};
+SQL_DB_PASS=${SQL_DB_PASS:-password};
 HTTP_PROTO=${HTTP_PROTO:-http};
 HTTP_URL=${HTTP_URL:-localhost};
+USER_NAME=${USER_NAME:-user};
+USER_PASS=${USER_PASS:-password};
+USER_EMAIL=${USER_EMAIL:-example@example.local};
 
-chown -R www-data:www-data /var/www/html/appserver 
-chmod -R 755 /var/www/html/* 
+chown -R www-data:www-data /var/www/html/appserver ;
+chmod -R 755 /var/www/html/* ;
 
-sed -i "s/>dbtype.*/>dbtype    = '${SQL_DB_TYPE}';/g" /var/www/html/appserver/config.php 
-sed -i "s/>dblibrary.*/>dblibrary = 'native';/g" /var/www/html/appserver/config.php 
-sed -i "s/>dbhost.*/>dbhost = '${SQL_DB_HOST}';/g" /var/www/html/appserver/config.php 
-sed -i "s/>dbname.*/>dbname = '${SQL_DB_NAME}';/g" /var/www/html/appserver/config.php 
-sed -i "s/>dbuser.*/>dbuser = '${SQL_DB_USER}';/g" /var/www/html/appserver/config.php 
-sed -i "s/>dbpass.*/>dbpass = '${SQL_DB_PASS}';/g" /var/www/html/appserver/config.php 
-sed -i "s/>wwwroot.*/>wwwroot = '${HTTP_PROTO}:\/\/${HTTP_URL}';/g" /var/www/html/appserver/config.php 
-sed -i "s/>dataroot.*/>dataroot = '\/var\/www\/html\/appserverdata';/g" /var/www/html/appserver/config.php 
+if [ -f "/run.sh" ]; then
+    chmod a+x /run.sh ;
+    source /run.sh ;
+else
+    echo "Script '/run.sh' is missing!";
+fi
 
 service php8.1-fpm start
 service nginx start
 
+cat /var/log/run-script.log
 tail -f /var/log/nginx/*.log &
 wait $!
