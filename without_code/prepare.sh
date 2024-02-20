a8fbc5a176e8c35be0cb96b4c66a5cef1669dada
Alex Ivanov <ivanov.alexey@onlyoffice.com>
2024-02-20 12:54:07 +0300

    Update prepare.sh

--- without_code/prepare.sh
+++ without_code/prepare.sh
@@ -2,7 +2,7 @@
 
 MOODLE_VERSION=${MOODLE_VERSION:-MOODLE_403_STABLE};
 
-docker build -t alste/php8.1-nocode:v0.1 .
+docker build -t alste/php8.1-nocode:v0.2 .
 
 apt-get install -y git ;
 git clone -b ${MOODLE_VERSION} git://git.moodle.org/moodle.git ./moodle_git ;
@@ -12,4 +12,7 @@ cp -f ./moodle_git/config-dist.php ./moodle_git/config.php
 #docker run -i -t -d -p 8080:8080 --name appserver --restart=always \
 #    -v ${PWD}/moodle_git:/var/www/html/appserver \
 #    -v ${PWD}/run.sh:/run.sh \
-#    alste/php8.1-nocode:v0.1
+#    -e DEBUG_SCRIPT=true \
+#        -e HTTP_PROTO=http \
+#            -e HTTP_URL=domain.name \
+#    alste/php8.1-nocode:v0.2
